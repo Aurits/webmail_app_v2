d833fc975d55b21bdfe0f8d4d422e1f70d226d93
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-25 07:10:37 +0000

    Nov 25, 2023, 10:10 AM

--- lib/models/mail.dart
+++ lib/models/mail.dart
@@ -142,8 +142,6 @@ class Mail {
     }
 
     try {
-      //DELETE TABLE
-      await db.execute('''DROP TABLE IF EXISTS emails''');
       await db.execute('''
                   CREATE TABLE IF NOT EXISTS emailTable (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
