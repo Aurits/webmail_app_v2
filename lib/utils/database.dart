5dc11a24b8fbb9919e8c9991ac1540fb5b84eea0
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-25 08:26:32 +0000

    Nov 25, 2023, 11:26 AM

--- lib/utils/database.dart
+++ lib/utils/database.dart
@@ -2,7 +2,20 @@ import 'package:sqflite/sqflite.dart';
 
 class Utils {
   static init() async {
-    var db = openDatabase('local_db');
+    var db = openDatabase('local.db', version: 1, onCreate: (db, version) {
+      db.execute('''
+        CREATE TABLE emails(
+          id TEXT PRIMARY KEY,
+          sender TEXT,
+          receiver TEXT,
+          reply_to TEXT,
+          date TEXT,
+          subject TEXT,
+          message TEXT,
+          attachments TEXT
+        )
+      ''');
+    });
     return db;
   }
 }
