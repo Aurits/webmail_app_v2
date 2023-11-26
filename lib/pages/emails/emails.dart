86c55ac7030a937e79111c4e080ab698d35199bb
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-26 02:12:18 +0000

    Nov 26, 2023, 5:12 AM

--- lib/pages/emails/emails.dart
+++ lib/pages/emails/emails.dart
@@ -1,7 +1,7 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:makerere_webmail_app/widgets/bottom_navigation.dart';
-
 import '../../models/mail.dart';
 
 class EmailsPage extends StatefulWidget {
@@ -11,15 +11,25 @@ class EmailsPage extends StatefulWidget {
   State<EmailsPage> createState() => _EmailsPageState();
 }
 
-class _EmailsPageState extends State<EmailsPage> {
+class _EmailsPageState extends State<EmailsPage>
+    with SingleTickerProviderStateMixin {
   List<Mail> emails = [];
 
   bool loading = false;
+  TabController? _tabController;
 
   @override
   void initState() {
     super.initState();
     _loadEmails();
+    _tabController = TabController(length: 2, vsync: this);
+    _tabController!.addListener(() {});
+  }
+
+  @override
+  void dispose() {
+    _tabController!.dispose();
+    super.dispose();
   }
 
   // Function to load emails
@@ -63,78 +73,89 @@ class _EmailsPageState extends State<EmailsPage> {
         ),
       ),
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Stack(
-          children: [
-            Column(
-              children: [
-                const SizedBox(height: 10),
-                // Display emails
-                Expanded(
-                  child: RefreshIndicator(
-                    onRefresh: _handleRefresh,
-                    child: ListView.builder(
-                      itemCount: emails.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        Mail email = emails[index];
-                        // Customize the ListTile according to your email model
-                        return ListTile(
-                          title: Text(email.subject),
-                          subtitle: Text(email.sender),
-                          // Add more details as needed
-                        );
-                      },
-                    ),
-                  ),
-                ),
-                // Display the number of emails
-                Text(
-                  "You have ${emails.length} emails",
-                  style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                // InkWell to refresh emails
-                InkWell(
-                  onTap: _handleRefresh,
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.refresh,
-                          color: Colors.white,
-                        ),
-                        SizedBox(width: 10),
-                        Text(
-                          "Refresh",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: BottomNavigation(),
-            ),
-          ],
+      //floating
+      floatingActionButton: Container(
+        margin: const EdgeInsets.all(10),
+        child: FloatingActionButton(
+          heroTag: "fab1",
+          backgroundColor: Colors.teal[500],
+          onPressed: () {
+            print("Pressed");
+          },
+          elevation: 2,
+          child: const Icon(
+            Icons.edit,
+            color: Colors.white,
+          ),
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: RefreshIndicator(
+              onRefresh: _handleRefresh,
+              child: ListView.builder(
+                itemCount: emails.length,
+                itemBuilder: (BuildContext context, int index) {
+                  Mail email = emails[index];
+                  // Customize the ListTile according to your email model
+                  return ListTile(
+                    title: Text(email.subject),
+                    subtitle: Text(email.sender),
+                    // Add more details as needed
+                  );
+                },
+              ),
+            ),
+          ),
+          const Expanded(
+            child: Text(
+              "Hello",
+            ),
+          ),
+          Card(
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0),
+            ),
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            margin: const EdgeInsets.all(0),
+            color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 4,
+              ),
+              child: TabBar(
+                indicatorColor: Colors.transparent,
+                indicatorSize: TabBarIndicatorSize.tab,
+                indicatorWeight: 1,
+                tabs: [
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 0, 15, 0),
+                    child: Tab(
+                      icon: Icon(
+                        Icons.apps,
+                        color: Colors.teal[600],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 0, 15, 0),
+                    child: Tab(
+                      icon: Icon(
+                        Icons.settings_applications,
+                        color: Colors.teal[600],
+                      ),
+                    ),
+                  ),
+                ],
+                controller: _tabController,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
