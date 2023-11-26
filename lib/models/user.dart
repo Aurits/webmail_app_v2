caf524576c68cf441a279e44cb41b41e4b5cfe3f
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-26 03:59:51 +0000

    Nov 26, 2023, 6:59 AM

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,9 @@
+// ignore_for_file: avoid_print
+
+import 'package:sqflite/sqflite.dart';
+
+import '../utils/database.dart';
+
 class User {
   final String username;
   final String password;
@@ -13,5 +19,67 @@ class User {
 
   Map<String, dynamic> toJson() {
     return {'username': username, 'password': password};
+  }
+
+//save user in local database //
+  Future<void> save() async {
+    Database db = await Utils.init();
+    String resp = await initTable(db);
+
+    print(resp);
+    print("start......next");
+
+    if (resp.isNotEmpty) {
+      print("start......");
+      try {
+        await db.insert(
+          'userTable',
+          toJson(),
+          conflictAlgorithm: ConflictAlgorithm.replace,
+        );
+        print("Saved successfully");
+      } catch (e) {
+        print("Failed db save: $e");
+      }
+    } else {
+      print("Table not created");
+    }
+  }
+
+  // Initialize the database table
+  static Future<String> initTable(Database db) async {
+    String resp = '';
+
+    if (db == null) {
+      resp = 'Failed to initialize the db';
+      print(resp);
+      return resp;
+    }
+
+    try {
+      await db.execute('''
+        CREATE TABLE IF NOT EXISTS userTable(
+          username TEXT PRIMARY KEY,
+          password TEXT NOT NULL
+        )
+      ''');
+
+      resp = 'Table for user created successfully....';
+      print(resp);
+    } catch (e) {
+      resp = 'Failed to create table in the db: ${e.toString()}}';
+      print(resp);
+    }
+    return resp;
+  }
+
+  //TO DISPLAY THE NAME OF THE EMAIL IN THE DATABASE BY PRINT
+  static void deleteUser() async {
+    Database db = await Utils.init();
+    await initTable(db);
+
+    db.delete('userTable');
+
+    print('deleted');
   }
 }
