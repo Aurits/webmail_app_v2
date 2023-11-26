08bd558e3e337869157d245b9333aac569626f9d
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-26 03:37:35 +0000

    Nov 26, 2023, 6:37 AM

--- lib/pages/emails/emails.dart
+++ lib/pages/emails/emails.dart
@@ -224,4 +224,54 @@ class _EmailsPageState extends State<EmailsPage>
       ),
     );
   }
+
+  void showBottonSheet() {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return Container(
+            color: Colors.white,
+            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+            child: Wrap(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(
+                    Icons.visibility,
+                  ),
+                  title: Text("Preview"),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.send,
+                  ),
+                  title: Text("Send"),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.person_add,
+                  ),
+                  title: Text("Share"),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.link,
+                  ),
+                  title: Text("Get Link"),
+                  onTap: () {},
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.content_copy,
+                  ),
+                  title: Text("Make a Copy"),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          );
+        });
+  }
 }
