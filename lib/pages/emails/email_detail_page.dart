0b97e573c2a010df24fb41925217bab5050be135
Ambrose Alanda <alandaambrose@gmail.com>
2023-11-28 10:57:18 +0000

    Nov 28, 2023, 1:57 PM

--- lib/pages/emails/email_detail_page.dart
+++ lib/pages/emails/email_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import '../../models/mail.dart';
 
 class EmailDetailPage extends StatelessWidget {
@@ -13,7 +14,7 @@ class EmailDetailPage extends StatelessWidget {
         title: const Text('Email Details'),
         backgroundColor: Colors.green[400],
       ),
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -21,7 +22,7 @@ class EmailDetailPage extends StatelessWidget {
             Text(
               email.subject,
               style: const TextStyle(
-                fontSize: 24,
+                fontSize: 22,
                 fontWeight: FontWeight.bold,
               ),
             ),
@@ -32,13 +33,50 @@ class EmailDetailPage extends StatelessWidget {
             ),
             const SizedBox(height: 8),
             Text(
+              'To: ${email.receiver}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Reply-To: ${email.replyTo}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 8),
+            Text(
               'Date: ${email.date}',
               style: const TextStyle(fontSize: 16),
             ),
             const SizedBox(height: 16),
-            Text(
-              email.message,
-              style: const TextStyle(fontSize: 18),
+            Html(data: email.message),
+            const SizedBox(height: 16),
+            if (email.attachments.isNotEmpty)
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Attachments:',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(email.attachments),
+                ],
+              ),
+            const SizedBox(height: 32),
+            // Footer
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              color: Colors.grey[200],
+              child: Text(
+                'This email is brought to you by Your Email App',
+                style: TextStyle(
+                  color: Colors.grey[600],
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
             ),
           ],
         ),
